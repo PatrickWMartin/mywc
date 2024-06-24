dd2abf476b15a48969f776e45fa3a5d2d22d6351
Patrick Martin <patrickwaltermartin@gmail.com>
2024-06-24 11:58:41 -0300

    Add -c flag functionality

--- main.go
+++ main.go
@@ -3,8 +3,28 @@ package main
 import (
     "fmt"
     "os"
+    "errors"
 )
+
+func ParseCommandLineArgs(argsList []string) error{
+
+    if len(argsList) == 0 {
+        return errors.New("no arguments provided")
+    }
+
+    if argsList[0] == "-c"{
+        file, err := os.Stat(argsList[1]) 
+        if err != nil {
+            return err
+        }
+
+        fmt.Println(file.Size(), argsList[1])
+    }
+
+    return nil
+}
+
 func main() {
     commandArgs := os.Args[1:]
-    fmt.Println(commandArgs) 
+    ParseCommandLineArgs(commandArgs)
 }
