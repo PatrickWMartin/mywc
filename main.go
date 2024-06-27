f4eaf1ad10f6846ba111b576d8703c7139ae5c49
Patrick Martin <patrickwaltermartin@gmail.com>
2024-06-27 19:09:02 -0300

    Refactor out the file opening and scanner logic to reduce similar code

--- main.go
+++ main.go
@@ -27,57 +27,63 @@ func ParseCommandLineArgs(argsList []string) error {
 	return nil
 }
 
-func GetCharacterCount(path string) error {
+func open_file(path string) (*os.File, func()){
     file, err := os.Open(path)
     if err != nil {
-        return err
+        panic(fmt.Sprintf("error: %v", err))
     }
-    defer file.Close()
 
-    fileScanner := bufio.NewScanner(file)
-    fileScanner.Split(bufio.ScanRunes)
-    charCount := 0
+    closeFn := func() {
+        err := file.Close()
+        if err != nil {
+            panic("failed to close input file")
+        }
+    }
+
+    return file, closeFn
+}
+
+func get_count(file os.File, split bufio.SplitFunc) int  {
+    fileScanner := bufio.NewScanner(&file)
+    fileScanner.Split(split)
+    count := 0
 
     for fileScanner.Scan() {
-        charCount++
+        count++
     }
+
+    return count
+}
+
+func GetCharacterCount(path string) error {
+
+    file, closeFn := open_file(path)
+    
+    defer closeFn()
+    
+    charCount := get_count(*file, bufio.ScanRunes)
 
     fmt.Println(charCount, path)
     return nil
 }
 
 func GetWordCount(path string) error {
-    file, err := os.Open(path)
-    if err != nil {
-        return err
-    }
-    defer file.Close()
+    file, closeFn := open_file(path)
     
-    fileScanner := bufio.NewScanner(file)
-    fileScanner.Split(bufio.ScanWords)
-    wordCount := 0
-
-    for fileScanner.Scan() {
-        wordCount++
-    }
+    defer closeFn()
+    
+    wordCount := get_count(*file, bufio.ScanWords)
 
     fmt.Println(wordCount, path)
     return nil
 }
 
 func GetFileLineCount(path string) error {
-    file, err := os.Open(path)
-    if err != nil {
-        return err
-    }
-    defer file.Close()
-
-    fileScanner := bufio.NewScanner(file)
-    lineCount := 0
-
-    for fileScanner.Scan() {
-        lineCount++
-    }
+    file, closeFn := open_file(path)
+    
+    defer closeFn()
+    
+    lineCount := get_count(*file, bufio.ScanLines)
 
     fmt.Println(lineCount, path)
     return nil
