5ab4b0306237c70fc52a2db74d0d9fcc551750a4
Patrick Martin <patrickwaltermartin@gmail.com>
2024-06-30 12:18:50 -0300

    Commiting becaue I am going to do a major refactor

--- main.go
+++ main.go
@@ -2,54 +2,103 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"os"
+    "io/fs"
 )
 
-func ParseCommandLineArgs(argsList []string) error {
+func checkForFlag(input string) bool {
+    if len(input) > 1 && input[0] == '-'{
+        return true
+    }
+    return false
+}
 
-	if len(argsList) == 0 {
-		return errors.New("no arguments provided")
-	}
+func checkStdIn() bool {
+    stat, err := os.Stdin.Stat()
+    if err != nil {
+        fmt.Fprintln(os.Stderr, "Error checking stdin:", err)
+        os.Exit(1)
+    }
+    if (stat.Mode() & os.ModeCharDevice) == 0 {
+        return true
+    }
 
-	if len(argsList) == 1 {
-		fileSize := GetFileSize(argsList[0])
+    return false
+}
+
+func ParseCommandLineArgs(argsList []string)  {
+
+    // has to have input from stdin or its invaled 
+    if len(argsList) == 0 {
+        if checkStdIn() == false {
+            fmt.Fprintln(os.Stderr, "Error no argument provided")
+        }
+        fileSize := GetFileSize("")
+		lineCount := GetFileLineCount("")
+		wordCount := GetWordCount("")
+		fmt.Println(lineCount, wordCount, fileSize)
+        os.Exit(0)
+    }
+
+    if len(argsList) == 1{
+        if checkForFlag(argsList[0]) && checkStdIn(){
+            executeFlag(argsList[0], "") 
+            os.Exit(0) 
+        }
+
+        fileSize := GetFileSize(argsList[0])
 		lineCount := GetFileLineCount(argsList[0])
 		wordCount := GetWordCount(argsList[0])
 		fmt.Println(lineCount, wordCount, fileSize, argsList[0])
-		return nil
-	}
+        os.Exit(0)
+    }
 
-	switch argsList[0] {
+    executeFlag(argsList[0], argsList[1]) 
+
+}
+
+func executeFlag(flag string, path string){
+    switch flag {
 	case "-c":
-		fileSize := GetFileSize(argsList[1])
-		fmt.Println(fileSize, argsList[1])
+		fileSize := GetFileSize(path)
+		fmt.Println(fileSize, path)
 	case "-l":
-		lineCount := GetFileLineCount(argsList[1])
-		fmt.Println(lineCount, argsList[1])
+		lineCount := GetFileLineCount(path)
+		fmt.Println(lineCount, path)
 	case "-w":
-		wordCount := GetWordCount(argsList[1])
-		fmt.Println(wordCount, argsList[1])
+		wordCount := GetWordCount(path)
+		fmt.Println(wordCount, path)
 	case "-m":
-		charCount := GetCharacterCount(argsList[1])
-		fmt.Println(charCount, argsList[1])
+		charCount := GetCharacterCount(path)
+		fmt.Println(charCount, path)
+    default:
+        fmt.Println("wc: illegal option -- ", flag)
 	}
 
-	return nil
 }
 
 func open_file(path string) (*os.File, func()) {
-	file, err := os.Open(path)
+    var file *os.File
+    var err error
+
+    if path == ""{
+        file = os.Stdin
+        err = nil
+    } else {
+        file, err = os.Open(path)
+    }
 	if err != nil {
 		panic(fmt.Sprintf("error: %v", err))
 	}
-
+    
 	closeFn := func() {
-		err := file.Close()
-		if err != nil {
-			panic("failed to close input file")
-		}
+        if path != ""{
+            err := file.Close()
+            if err != nil {
+                panic("failed to close input file")
+            }
+        }
 	}
 
 	return file, closeFn
@@ -99,13 +148,20 @@ func GetFileLineCount(path string) int {
 }
 
 func GetFileSize(path string) int64 {
-	file, err := os.Stat(path)
+    var file fs.FileInfo
+    var err error
+    if path == ""{
+        file, err = os.Stdin.Stat()
+    } else {
+        file, err = os.Stat(path)
+    }
 	if err != nil {
 		panic("Error getting file size")
 	}
 
 	return file.Size()
 }
+
 
 func main() {
 	commandArgs := os.Args[1:]
