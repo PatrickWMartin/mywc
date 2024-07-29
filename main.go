3ecbd77d2e9fa15456938734411819af76275c09
Patrick Martin <patrickwaltermartin@gmail.com>
2024-07-29 13:10:31 -0300

    Add word,line,byte, and character count functions

--- main.go
+++ main.go
@@ -8,28 +8,45 @@ import (
 )
 
 func checkStdIn() bool {
-    stat, err := os.Stdin.Stat()
-    if err != nil {
-        fmt.Fprintln(os.Stderr, "Error checking stdin:", err)
-        os.Exit(1)
-    }
-    if (stat.Mode() & os.ModeCharDevice) == 0 {
-        return true
-    }
+	stat, err := os.Stdin.Stat()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error checking stdin:", err)
+		os.Exit(1)
+	}
+	if (stat.Mode() & os.ModeCharDevice) == 0 {
+		return true
+	}
 
-    return false
+	return false
 }
 
-func main(){
-    var bytes []byte
-    if checkStdIn() {
-        bytes,_ = io.ReadAll(os.Stdin)
-    } else {
-        bytes,_ = os.ReadFile("test.txt") // just pass the file name
-    }
-    test := string(bytes)
-    fmt.Println(len(bytes))
-    fmt.Println(len(strings.Split(test, "")))
-    fmt.Println(len(strings.Fields(test)))
-    fmt.Println(len(strings.Split(test, "\n"))-1)
+func getBytes(input []byte) int {
+	return len(input)
+}
+
+func getWords(input string) int {
+	return len(strings.Fields(input))
+}
+
+func getChatacters(input string) int {
+	return len(strings.Split(input, ""))
+}
+
+func getLines(input string) int {
+	return len(strings.Split(input, "\n")) - 1
+}
+
+func main() {
+	var bytes []byte
+	if checkStdIn() {
+		bytes, _ = io.ReadAll(os.Stdin)
+	} else {
+		bytes, _ = os.ReadFile("test.txt") // just pass the file name
+	}
+
+	fmt.Println(getBytes(bytes))
+	input := string(bytes)
+	fmt.Println(getWords(input))
+	fmt.Println(getChatacters(input))
+	fmt.Println(getLines(input))
 }
